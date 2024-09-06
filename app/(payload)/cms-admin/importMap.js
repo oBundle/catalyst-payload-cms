cd57983de99ad6103488d0637147516346423757
Peter Valadez <valadezpm@gmail.com>
2024-09-06 00:25:06 -0400

    Move payload admin url from cms-admin back to admin to resolve an issue with payload beta

--- app/(payload)/cms-admin/importMap.js
+++
@@ -1,5 +0,0 @@
-import { emptyComponent as emptyComponent_0 } from '@payloadcms/ui/shared'
-
-export const importMap = {
-  '@payloadcms/ui/shared#emptyComponent': emptyComponent_0,
-}
--- payload.config.ts
+++ payload.config.ts
@@ -1,16 +1,16 @@
 // storage-adapter-import-placeholder
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { sqliteAdapter } from '@payloadcms/db-sqlite';
+import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import path from 'path';
+import { buildConfig } from 'payload';
+import sharp from 'sharp';
+import { fileURLToPath } from 'url';
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Media } from './collections/Media';
+import { Users } from './collections/Users';
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -32,10 +32,10 @@ export default buildConfig({
   }),
   routes: {
     api: '/cms-api',
-    admin: '/cms-admin',
+    admin: '/admin',
   },
   sharp,
   plugins: [
     // storage-adapter-placeholder
   ],
-})
+});
